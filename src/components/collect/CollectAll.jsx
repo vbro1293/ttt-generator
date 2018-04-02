77c992e1aba72c7ff5e1be733095b0a17ef44552
Vikki <vbro1293@gmail.com>
2018-04-02 16:19:34 +0100

    input validation added - no empty input or repeated name

--- src/components/collect/CollectAll.jsx
+++ src/components/collect/CollectAll.jsx
@@ -19,6 +19,8 @@ class Collect extends Component {
 
 		//bind methods to this
 		this.addPlayer = this.addPlayer.bind(this);
+		this.isDisabled = this.isDisabled.bind(this);
+
 	}
 
 	//Method called when "add player" button pressed - adds input value to players list
@@ -38,13 +40,19 @@ class Collect extends Component {
 		})
 	}
 
+	//Method called for input validation - no empty field and no repeated name else isDisabled is true
+	isDisabled() {
+		const { newPlayer, players } = this.state;
+		return (!newPlayer || (players.filter(player => player === newPlayer)).length!==0);
+	}
+
 	render(){
 		const { newPlayer, players } = this.state;
 		return (
 			<main className="main">
 				<section className="add-players">
 					<Input onChange={ e => this.inputChange(e) } value={ newPlayer }/>
-					<Button onClick={ this.addPlayer } className="button">Add Player</Button>
+					<Button onClick={ this.addPlayer } isDisabled= { this.isDisabled() } className="button">Add Player</Button>
 				</section>
 				<PlayersList players={ players }/>
 				<Link to={ "/generated-tournament" } className="button">

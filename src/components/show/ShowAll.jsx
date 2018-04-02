18cf7d0630cd889cd0e459643d3e51f26c0f6506
Vikki <vbro1293@gmail.com>
2018-04-02 17:37:31 +0100

    randomised list  working

--- src/components/show/ShowAll.jsx
+++ src/components/show/ShowAll.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
-const ShowAll = () => (
-	<p>YES</p>
-)
+class ShowAll extends Component {
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {
+	  	players: ["p1" , "p2", "p3", "p4"]
+	  };
+	}
+
+
+	render () {
+		const { players } = this.state;
+		let playersLeft = players.slice(); //retwritten
+		let ranPlayers = [];
+		let length = players.length
+
+		//Randomise array of players
+		while(length>0){
+			const i = Math.floor(Math.random()*playersLeft.length);
+			const player = playersLeft.splice( i, 1 )[0];
+			ranPlayers.push(player);
+			length -= 1;
+		}
+
+		return (
+			<section>
+				{ ranPlayers.map( (player, i) => (
+					<p key={ i }>{ player }</p>
+				))}
+			</section>
+		)
+	}
+}
 
 export default ShowAll;

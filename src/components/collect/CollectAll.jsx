50320715b0a0eef5ae52cd4df653e8e51c82a439
Vikki <vbro1293@gmail.com>
2018-04-02 13:16:01 +0100

    Created playerslist and item components, imported into page

--- src/components/collect/CollectAll.jsx
+++ src/components/collect/CollectAll.jsx
@@ -1,17 +1,18 @@
 //Collect page - where players are added. Contains imported components
 import React from "react";
 
-
-//Import components
+//================Import components
 import Input from "./Input";
 import Button from "./Button";
+import PlayersList from "./PlayersList";
 
 const Collect = () => (
-	<section>
+	<section className="main">
 		<form className="add-players">
 			<Input />
 			<Button>Add Player</Button>
 		</form>
+		<PlayersList />
 		<Button>Generate Tournament</Button>
 	</section>
 )
---
+++ src/components/collect/PlayersList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+//===============Import components
+import PlayerItem from "./PlayerItem";
+
+const PlayersList = () => (
+	<ul>
+		<PlayerItem>player1</PlayerItem>
+	</ul>
+)
+
+export default PlayersList;

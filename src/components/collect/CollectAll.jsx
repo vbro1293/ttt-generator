e82b31ee353788740e12403c7c73bf2ae834eb3b
Vikki <vbro1293@gmail.com>
2018-04-02 14:04:55 +0100

    changed CollectAll to Component and added local state

--- src/components/collect/CollectAll.jsx
+++ src/components/collect/CollectAll.jsx
@@ -1,20 +1,34 @@
 //Collect page - where players are added. Contains imported components
-import React from "react";
+import React, { Component } from "react";
 
 //================Import components
 import Input from "./Input";
 import Button from "./Button";
 import PlayersList from "./PlayersList";
 
-const Collect = () => (
-	<section className="main">
-		<form className="add-players">
-			<Input />
-			<Button>Add Player</Button>
-		</form>
-		<PlayersList />
-		<Button>Generate Tournament</Button>
-	</section>
-)
+class Collect extends Component {
+	constructor(props) {
+		super(props);
+
+		//create local state to store players
+		this.state = {
+			players: [],
+		}
+	}
+
+	render(){
+		return (
+			<section className="main">
+				<form className="add-players">
+					<Input />
+					<Button>Add Player</Button>
+				</form>
+				<PlayersList />
+				<Button>Generate Tournament</Button>
+			</section>
+		)
+	}
+	
+}
 
 export default Collect;

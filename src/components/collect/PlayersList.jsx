e3a8d133ae409cb926a9e377de7a606947fbdfbe
Vikki <vbro1293@gmail.com>
2018-04-03 21:45:16 +0100

    added non functional reset butttoon, conditionally shows if players exist

--- src/components/collect/PlayersList.jsx
+++ src/components/collect/PlayersList.jsx
@@ -2,13 +2,21 @@
 
 import React from "react";
 import PlayerItem from "./PlayerItem";
+import Button from "./Button";
 
 const PlayersList = ({ players }) => (
-	<ul>
-		{ players.map((player, i) => 
-			<PlayerItem key={ i }>{ player }</PlayerItem>
-		)}
-	</ul>
+	<section>
+		<ul>
+			{ players.map((player, i) => 
+				<PlayerItem key={ i }>{ player }</PlayerItem>
+			)}
+		</ul>
+		{ players.size ? 
+			<Button>Reset</Button>
+		:
+			null
+		}
+	</section>
 )
 
 export default PlayersList;

0a863629453736e4def0249d0e615d59886546b5
Vikki <vbro1293@gmail.com>
2018-04-03 22:20:32 +0100

    addedd subheader component

--- src/components/collect/CollectAll.jsx
+++ src/components/collect/CollectAll.jsx
@@ -3,12 +3,14 @@
 import React from "react";
 
 //================Import components
+import SubHeader from "../SubHeader";
 import PlayersList from "../../containers/PlayersList";
 import AddPlayer from "../../containers/AddPlayer";
 import GenerateLink from "../../containers/GenerateLink";
 
 const Collect = () => (
 	<main className="main">
+		<SubHeader>Fill in each players name in the box below and click generate to reveal your dream tournament</SubHeader>
 		<AddPlayer />
 		<PlayersList />
 		<GenerateLink>Generate Tournament</GenerateLink>

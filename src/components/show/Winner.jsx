1ba8b7e75e3d120abad0544890a7747209b0fd99
Vikki <vbro1293@gmail.com>
2018-04-07 08:28:23 +0100

    created winner component

---
+++ src/components/show/Winner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Winner = () => (
+	<div className="winner">
+		<h2><span role="img" aria-label="trophy">🏆</span> Winner! <span role="img" aria-label="trophy">🏆</span></h2>
+	</div>
+)
+
+export default Winner;

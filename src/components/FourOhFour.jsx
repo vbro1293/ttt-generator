8e1fd09cfb12c28d962f6c1dad6ca36ac62dbd44
Vikki <vbro1293@gmail.com>
2018-04-08 20:04:08 +0100

    404 page, favicon last tidying

--- src/components/FourOhFour.jsx
+++ src/components/FourOhFour.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 
 // 404 component
-const FourOhFour = () => <p>Nothing found</p>;
+const FourOhFour = () => (
+	<section>
+		<h3>
+			<span role="img" aria-label="snail"> 🐌 </span>
+			404 
+			<span role="img" aria-label="snail"> 🐌 </span>
+		</h3>
+		<p>Sorry this page does not exist. Keep looking. </p>
+		<p>As long as you try,  you'll find what you're searching for...</p>
+		<p>Never give up hope.
+		</p>
+	</section>
+);
 
 export default FourOhFour;

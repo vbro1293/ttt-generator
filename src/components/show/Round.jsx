db9af6958b71bec1e0d6e447ef6de1ab405d762e
Vikki <vbro1293@gmail.com>
2018-04-08 17:28:59 +0100

    added flexbox to larger screen

--- src/components/show/Round.jsx
+++ src/components/show/Round.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Match from "./Match";
 
 const Round = ({ round, roundNum, onClick }) => (
-	<div className="round">
+	<section className="wholeRound">
 		<h2>{ round.length!==1 ? "Round "+roundNum : "The Final" }</h2>
-		{ round.map((match, j) => 
-				<Match key={ j } bye={ match.bye } player1={ match.p1 } player2={ match.p2} roundNum={ roundNum } matchInd={ j} onClick={ onClick } />
-		)}
-	</div>
+		<div className="round">
+			{ round.map((match, j) => 
+					<Match key={ j } bye={ match.bye } player1={ match.p1 } player2={ match.p2} roundNum={ roundNum } matchInd={ j} onClick={ onClick } />
+			)}
+		</div>
+	</section>
 )
 
 export default Round;
